1979e634d4ad0e9e6b22c39f8458e84e2be66de4
eabrawer <ericbrawer@gmail.com>
2015-04-19 16:38:03 -0400

    set route index to home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'pages/index'
+  # get 'pages/index'
 
   get 'pages/show'
 
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'pages#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
